c9c93b664288423e4f3ee9ee1ddc3fd1c0c7c929
th3-s7r4ng3r <gvinura@gmail.com>
2025-12-03 22:54:34 +0530

    Ref: updated constant vars names

--- constants.ts
+++ constants.ts
@@ -1,5 +1,5 @@
 
-export const USER_API_BASE_URL = window._env_?.USER_API_BASE_URL || import.meta.env.VITE_USER_API_BASE_URL || 'https://studious-guide-555j7pqx57q34946-8081.app.github.dev/api';
-export const EVENT_API_BASE_URL = window._env_?.EVENT_API_BASE_URL || import.meta.env.VITE_EVENT_API_BASE_URL || 'https://studious-guide-555j7pqx57q34946-8082.app.github.dev/api';
-export const BOOKING_API_BASE_URL = window._env_?.BOOKING_API_BASE_URL || import.meta.env.VITE_BOOKING_API_BASE_URL || 'https://studious-guide-555j7pqx57q34946-8083.app.github.dev/api';
-export const TICKETING_API_BASE_URL = window._env_?.TICKETING_API_BASE_URL || import.meta.env.VITE_TICKETING_API_BASE_URL || 'https://studious-guide-555j7pqx57q34946-8084.app.github.dev/api';
+export const USER_API_BASE_URL = window._env_?.VITE_USER_API_BASE_URL || import.meta.env.VITE_USER_API_BASE_URL || 'https://studious-guide-555j7pqx57q34946-8081.app.github.dev/api';
+export const EVENT_API_BASE_URL = window._env_?.VITE_EVENT_API_BASE_URL || import.meta.env.VITE_EVENT_API_BASE_URL || 'https://studious-guide-555j7pqx57q34946-8082.app.github.dev/api';
+export const BOOKING_API_BASE_URL = window._env_?.VITE_BOOKING_API_BASE_URL || import.meta.env.VITE_BOOKING_API_BASE_URL || 'https://studious-guide-555j7pqx57q34946-8083.app.github.dev/api';
+export const TICKETING_API_BASE_URL = window._env_?.VITE_TICKETING_API_BASE_URL || import.meta.env.VITE_TICKETING_API_BASE_URL || 'https://studious-guide-555j7pqx57q34946-8084.app.github.dev/api';
